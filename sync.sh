c4ca8aef091610634d3f3cd7d2de879775fcb964
Dominik Madarasz <zaklaus@outlook.com>
2020-02-05 20:03:26 +0100

    Fix folder paths

--- sync.sh
+++ sync.sh
@@ -16,7 +16,7 @@ copy() {
 copy_dir() {
 	echo "Copying folder $1 into $REPO/$1 ..."
 	mkdir -p $REPO/$1
-	rsync -r --exclude=.git $1 $REPO/$1
+	rsync -r --exclude=.git $1 $REPO/
 }
 
 # home config files
