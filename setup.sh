5f914421c4420ec8c7acd2b54edeb2be57eac2a8
Dominik Madarasz <zaklaus@outlook.com>
2021-12-12 19:15:42 +0100

    update

--- setup.sh
+++ setup.sh
@@ -8,20 +8,12 @@ cd $HOME
 echo "Repo location: $REPO"
 echo "Source location: $HOME"
 
-# Install Oh My Zsh!
-
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/k
-
 # Copy base files to $HOME dir
 
 cp -r $REPO/. $HOME/
 
 # Compile and install dwm, dmenu and st
 
-mkdir -p $HOME/bin
 $HOME/suckless/package.sh
 
 # Install Tmux Plugin Manager
